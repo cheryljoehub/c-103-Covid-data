004bc909a3af2e3bcfdf0712a7696b4d6148d45b
joe's hub <53526094+cheryljoehub@users.noreply.github.com>
2020-09-01 14:55:17 +0530

    Update covidData.py

--- covidData.py
+++ covidData.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv("data.csv")
+df = pd.read_csv("data - data.csv")
 
 fig = px.scatter(df,    x="date",
                         y="cases",
